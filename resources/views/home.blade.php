e012d6fd54aa6e5a9d8dcaf350fb2d1d7e644b40
Yuri Alves <yurialvesguernsey@outlook.com>
2022-04-26 23:04:07 +0100

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,11 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
                 </div>
             </div>
         </div>
