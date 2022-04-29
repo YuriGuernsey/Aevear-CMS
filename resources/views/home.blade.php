dd69aa2ebe3acaae7523f3f46ae6b5ab00371637
Yuri Alves <yurialvesguernsey@outlook.com>
2022-04-29 17:50:05 +0100

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,17 +29,7 @@
         </div>
     </div>
 
-    @if (session('status'))
-<div class="alert alert-success" role="alert">
-	<button type="button" class="close" data-dismiss="alert">×</button>
-	{{ session('status') }}
-</div>
-@elseif(session('failed'))
-<div class="alert alert-danger" role="alert">
-	<button type="button" class="close" data-dismiss="alert">×</button>
-	{{ session('failed') }}
-</div>
-@endif
+   
 <form action = "{{ route('logout') }}" method = "post">
 	<input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
 	<table>
--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::post('create','SiteInserController@insert');
+
