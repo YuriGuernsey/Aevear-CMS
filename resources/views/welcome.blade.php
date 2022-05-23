65781acba2942b25bf538170770d9bfd46e13f26
Yuri Alves <yurialvesguernsey@outlook.com>
2022-05-23 21:48:28 +0100

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,14 +87,12 @@
         <div class="sm:text-center lg:text-left">
           <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span class="block xl:inline">  Get performance-driven and beautiful</span>
-            <span class="block text-indigo-600 xl:inline">  <a href="/services">
-      <div class="mask">
+            <span class="block text-indigo-600 xl:inline"><div class="mask">
           <span data-show>Websites</span>
         <span>Mobile Applications</span>
         <span>Videos</span>
         <span>Google Sheets</span>
-      </div>
-        </a></span>
+      </div></span>
         for half the cost.
           </h1>
           <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
@@ -118,7 +116,7 @@
 </header>
 <style>
     .mask {
-    height: 45px;
+    height: 65px;
     position: relative;
     overflow: hidden;
     margin-top: var(--offset);
@@ -128,7 +126,7 @@
     display: block;
     box-sizing: border-box;
     position: absolute;
-    top: 50px;
+    top: 60px;
     padding-bottom: var(--offset);
     
     background-size: 100% 100%;
