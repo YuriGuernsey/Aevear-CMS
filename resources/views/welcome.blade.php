190d0eb7a12fb05d8aff6961aefb6e0e689cc693
Yuri Alves <yurialvesguernsey@outlook.com>
2022-05-23 21:57:52 +0100

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,13 +86,15 @@
       <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div class="sm:text-center lg:text-left">
           <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline">  Get performance-driven and beautiful</span>
-            <span class="block text-indigo-600 xl:inline"><div class="mask">
-          <span data-show>Websites</span>
-        <span>Mobile Applications</span>
-        <span>Videos</span>
-        <span>Google Sheets</span>
-      </div></span>
+            <span class="block xl:inline">Get performance-driven and beautiful</span>
+            <span class="block text-indigo-600 xl:inline">
+              <div class="mask">
+                <span data-show>Websites</span>
+                <span>Mobile Applications</span>
+                <span>Videos</span>
+                <span>Google Sheets</span>
+              </div>
+            </span>
         for half the cost.
           </h1>
           <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
@@ -115,7 +117,7 @@
 
 </header>
 <style>
-    .mask {
+.mask {
     height: 65px;
     position: relative;
     overflow: hidden;
